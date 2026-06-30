07334281af48cc41d3132a99039b3800af5d3428
David Riley <dmriley993@gmail.com>
2026-06-30 20:52:01 +1000

    Bump version to 0.2.0rc2 for pre-release publish

--- src/solve_nivp/__init__.py
+++ src/solve_nivp/__init__.py
@@ -50,7 +50,7 @@ See the Sphinx documentation (``docs/``) for extended examples.
 
 import numpy as np
 
-__version__ = "0.2.0.dev4"
+__version__ = "0.2.0rc2"
 
 from .projections import (
   Projection,
